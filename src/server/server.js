f81ae7ae01dfe3e2b60c1210321cddfe74063295
Nanda Kresnatara <nandayavanets@mail.ru>
2025-05-25 21:41:06 +0700

    added: server to create bot

---
+++ src/server/server.js
@@ -0,0 +1,13 @@
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import 'dotenv/config'
+
+const bot = new Telegraf(process.env.BOT_API_TOKEN)
+
+bot.start((ctx) => ctx.reply('Welcome to Finance Tracker'));
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+console.log("Bot is running on polling...")
