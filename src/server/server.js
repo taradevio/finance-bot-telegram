23c1d96bd8b8879799446e9d954d58f0e4a4cc90
Nanda Kresnatara <nandayavanets@mail.ru>
2025-05-25 22:12:30 +0700

    added: custom command

--- src/server/server.js
+++ src/server/server.js
@@ -5,9 +5,14 @@ import 'dotenv/config'
 const bot = new Telegraf(process.env.BOT_API_TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome to Finance Tracker'));
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.command('income', (ctx) => ctx.reply('Add your income'))
+bot.command('outcome', (ctx) => ctx.reply('What did you spend on today?'))
+
+
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
 console.log("Bot is running on polling...")
